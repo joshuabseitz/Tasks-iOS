03840f45e2538f1b3d8afe00fd383a2000776b79
Joshua Seitz <jseitz2@luc.edu>
2020-10-13 17:56:43 -0500

    Added new UI and their properties for entry view, added description to the object model for tasks

--- Tasks/View Controllers/EntryViewController.swift
+++ Tasks/View Controllers/EntryViewController.swift
@@ -15,33 +15,67 @@ protocol EntryViewControllerDelegate: class {
 class EntryViewController: UIViewController {
 	
 	weak var delegate: EntryViewControllerDelegate?
-
-    @IBOutlet var textField: UITextField!
 	
-    
+	@IBOutlet weak var taskTitleTextView: UITextView!
+	@IBOutlet weak var taskDescriptionTextView: UITextView!
+	@IBOutlet weak var taskValueTextView: UITextView!
+	
     override func viewDidLoad() {
 		super.viewDidLoad()
 		self.title = "New Task"
-        textField.delegate = self
+		taskTitleTextView.delegate = self
 		
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save",
 															style: .done,
 															target: self,
 															action: #selector(saveTask))
+		
+		// taskTitleTextView Properties
+		taskTitleTextView.layer.cornerRadius = 5.0
+		taskTitleTextView.layer.borderColor = UIColor.black.cgColor
+		taskTitleTextView.layer.borderWidth = 0.75
+		taskTitleTextView.textContainerInset = UIEdgeInsets(top: 10,left: 5,bottom: 10,right: 5); // top, left, bottom, right
+		
+		// taskDescriptionTextView Properties
+		taskDescriptionTextView.layer.cornerRadius = 5.0
+		taskDescriptionTextView.layer.borderColor = UIColor.black.cgColor
+		taskDescriptionTextView.layer.borderWidth = 0.75
+		taskDescriptionTextView.textContainerInset = UIEdgeInsets(top: 10,left: 5,bottom: 10,right: 5); // top, left, bottom, right
+		
+		// taskValueTextView Properties
+		taskValueTextView.layer.cornerRadius = 5.0
+		taskValueTextView.layer.borderColor = UIColor.black.cgColor
+		taskValueTextView.layer.borderWidth = 0.75
+		taskValueTextView.textContainerInset = UIEdgeInsets(top: 10,left: 5,bottom: 10,right: 5); // top, left, bottom, right
+		
     }
     
     @objc func saveTask() {
 		// Dev note: objc allows it to be used as a selector
         
-        guard let text = textField.text, !text.isEmpty else {
-			print("Could not find text to create task.")
+        guard let taskTitle = taskTitleTextView.text, !taskTitle.isEmpty else {
+			print("Could not find task title.")
             return
         }
 		
+//		guard let taskValue = taskValueTextView.text, !taskValue.isEmpty else {
+//				   print("Could not find task value.")
+//				   return
+//	    }
+		
+		guard let taskDescription = taskDescriptionTextView.text, !taskDescription.isEmpty else {
+				   print("Could not find description.")
+				   return
+	    }
+		
 		let task = Task(context: PersistenceController.container.viewContext)
-		task.text = text
+		
+		task.text = taskTitle
+		task.descrip = taskDescription
+//		task.value = taskValue
 		task.dateCreated = Date()
 		task.completed = false
+		
 		print("Created task: \(task)")
 		delegate?.didFinishCreatingTask(task)
 
@@ -49,12 +83,12 @@ class EntryViewController: UIViewController {
     }
 }
 
-// MARK: - UITextFieldDelegate
+// MARK: - UITextViewDelegate
 
-extension EntryViewController: UITextFieldDelegate {
+extension EntryViewController: UITextViewDelegate {
 	
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        saveTask()
-        return false
-    }
+	func textViewDidBeginEditing(_ textView: UITextView) {
+		textView.text = ""
+	}
+
 }
